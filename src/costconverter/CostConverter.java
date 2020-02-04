59381774e05c71d124379b3fc611e40e20216d7f
norton287 <42098364+norton287@users.noreply.github.com>
2020-02-04 12:13:06 -0600

    Change currency rate

--- src/costconverter/CostConverter.java
+++ src/costconverter/CostConverter.java
@@ -75,10 +75,10 @@ public class CostConverter {
         Scanner in = new Scanner(System.in);
         
         //Convert USD values to AUD
-        travelCost = travelCost * 1.49523;
-        hotelCost = hotelCost * 1.49523;
-        rentalCarCost = rentalCarCost * 1.49523;
-        laborCost = laborCost * 1.49523;
+        travelCost = travelCost * 1.48426;
+        hotelCost = hotelCost * 1.48426;
+        rentalCarCost = rentalCarCost * 1.48426;
+        laborCost = laborCost * 1.48426;
         
         //Round up the computed numbers to the nearest dollar
         travelCost = Math.round(travelCost * 100.0) / 100.0;
@@ -87,7 +87,7 @@ public class CostConverter {
         laborCost = Math.round(laborCost * 100.0) / 100.0;
 
         //Output the AUD values using two decimal places in the String.format
-        System.out.println("\nCosts Converted to AUD From USD, Current AUD Rate is 1.49523 Per Dollar\n");
+        System.out.println("\nCosts Converted to AUD From USD, Current AUD Rate is 1.48426 Per Dollar\n");
         System.out.println("Travel Cost Converted To AUD is: " + String.format("%.2f", travelCost) + "\n"
                 + "Hotel Cost Converted To AUD is: " + String.format("%.2f", hotelCost) + "\n"
                 + "Rental Car Cost Converted To AUD is: " + String.format("%.2f", rentalCarCost) + "\n"
